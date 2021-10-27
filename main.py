884cbfb588d8e98ef3c5242ce32be0f79fd50240
imamhossain94 <imamagun94@gmail.com>
2021-10-27 18:59:14 +0600

    feat: scraping script completed

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from requests import get, post, Session
+from bs4 import BeautifulSoup
+import json
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_bus_data(url):
+    final_data = {'data': list()}
+    try:
+        html = get(url).text
+        tables = BeautifulSoup(str(html), 'html.parser').find_all('table')[0:]
+        global_data = []
+        for table in tables:
+            rows = BeautifulSoup(str(table), 'html.parser').find_all('tr')[0:]
+            for row in rows[0:]:
+                cols = row.find_all('td')
+                local_data = {}
+                if len(cols) != 0:
+                    bus_name_english = cols[0].text.split('(')[0].replace(' Bus Route', '').strip()
+                    bus_name_bangle = cols[0].text.split('(')[1].replace(')', '').strip()
+                    routes = cols[1].text.replace('See Full Route Map', '').split(' ⇄ ')
+                    time = cols[2].text
+                    service_type = cols[3].text
+                    local_data = {
+                        "english": bus_name_english,
+                        "bangle": bus_name_bangle,
+                        "routes": routes,
+                        "time": time,
+                        "service_type": service_type
+                    }
+                if local_data != {}:
+                    global_data.append(local_data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        final_data['data'] = global_data
+        print(json.dumps(final_data))
+    except Exception as ex:
+        final_data = {'status': 'failed', 'reason': str(ex)}
+        print(json.dumps(final_data))
+    return final_data
+
+
+get_bus_data("https://busroutebd.com/dhaka-local-bus-routes/")
